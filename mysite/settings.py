7c7d512ce010547e18582d8c413e523c287999af
Javier Abadía <javi.abadia@gmail.com>
2018-09-30 17:27:01 +0200

    idioma español

--- mysite/settings.py
+++ mysite/settings.py
@@ -86,7 +86,8 @@ DATABASES = {
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+# LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-es'
 
 TIME_ZONE = 'Europe/Madrid'
 
